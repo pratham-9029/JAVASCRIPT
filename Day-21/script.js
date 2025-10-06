65a8866672c788d8df9e73131ea06f21ca78a697
pratham-9029 <saixerox5075@gmail.com>
2025-10-06 17:11:50 +0530

    Done

---
+++ Day-21/script.js
@@ -0,0 +1,32 @@
+const loginForm = document.getElementById('loginForm');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+
+function validation(){
+    
+    
+    if (email.value.trim() === '') {
+        document.getElementById(`${email.name}-error`).classList.remove('d-none');
+    }
+    
+    if (password.value.trim() === '') {
+        document.getElementById(`${password.name}-error`).classList.remove('d-none');
+    }
+}
+
+
+
+
+
+// loginForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     if (email.value.trim() === '') {
+//         document.getElementById(`${email.name}-error`).classList.remove('d-none');
+//     }
+
+//     if (password.value.trim() === '') {
+//         document.getElementById(`${password.name}-error`).classList.remove('d-none');
+//     }
+// });
